1f3db04546d19b7bc2834c2581811bf987d51639
William Roy <wroy@proton.me>
2022-09-23 10:20:58 -0400

    Consistent name scheme for subtrees
    Signed-off-by: William Roy <wroy@proton.me>

--- update-subtrees.sh
+++ update-subtrees.sh
@@ -12,9 +12,9 @@ cd "$(dirname "$0")" || exit 1
 
 # Merge changes from upstream repositories.
 # Ideally, this should not be necessary, as we want to move the development here. 
-git subtree pull --prefix cairomm git@gitlab.gnome.org:wroy/cairomm.git master --squash
+git subtree pull --prefix mm-cairo git@gitlab.gnome.org:wroy/cairomm.git master --squash
 git subtree pull --prefix mm-common git@gitlab.gnome.org:wroy/mm-common.git master --squash
-git subtree pull --prefix glibmm git@gitlab.gnome.org:wroy/glibmm.git master --squash
-git subtree pull --prefix pangomm git@gitlab.gnome.org:wroy/pangomm.git master --squash
-git subtree pull --prefix gtkmm git@gitlab.gnome.org:wroy/gtkmm.git master --squash
+git subtree pull --prefix mm-glib git@gitlab.gnome.org:wroy/glibmm.git master --squash
+git subtree pull --prefix mm-pango git@gitlab.gnome.org:wroy/pangomm.git master --squash
+git subtree pull --prefix mm-gtk git@gitlab.gnome.org:wroy/gtkmm.git master --squash
 
